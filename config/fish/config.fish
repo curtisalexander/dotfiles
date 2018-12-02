854d8439305c4505a5048d961fd86c525c094b3c
Curtis Alexander <calex@calex.org>
2018-12-02 14:47:35 -0600

    add aliases

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -11,8 +11,15 @@ end
 # custom
 umask 022
 
+# aliases
 alias rm="rm -i"
 alias cls="clear"
+alias ll="ls -l"
+alias lla="ls -la"
+alias lla="ls -la"
+alias python="python3"
+alias pip="pip3"
+alias apt-update="sudo apt-get update; and sudo apt-get -y upgrade; and sudo apt-get clean"
 
 # term
 set term screen-256color
