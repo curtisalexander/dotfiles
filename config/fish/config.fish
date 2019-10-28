d9042c2fec7340a9b1cdc9e33d822c7e22629184
Curtis Alexander <calex@calex.org>
2019-10-28 18:34:38 -0500

    bootstrap fisher

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -41,3 +41,10 @@ else
     alias pip="pip3"
     alias apt-update="sudo apt-get update; and sudo apt-get -y upgrade; and sudo apt-get clean"
 end
+
+# bootstrap fisher and process fisher file
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
