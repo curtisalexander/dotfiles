a45d7badbfe9ae4647751468859bd3b1bdc609c1
Curtis Alexander <calex@calex.org>
2020-05-30 16:06:42 -0500

    updates

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -9,31 +9,33 @@ alias ll="exa --header --long"
 alias lla="exa --header --long -a"
 alias tree="exa --tree"
 alias grep="grep --color=auto"
+alias dir="exa --header"
 
 # macos vs windows subsystem for linux
 if test (uname) = "Darwin"
-    
+
     # homebrew
     set -gx HOMEBREW_NO_EMOJI 1
     set -gx HOMEBREW_NO_ANALYTICS 1
 
     # node
-    set -gx NPM_PACKAGES "$HOME/.npm-packages"
-    set -gx MANPATH "$NPM_PACKAGES/share/man:(manpath)"
-    set -gx NODE_PATH "$NPM_PACKAGES/lib/node_modules"
+    # set -gx NPM_PACKAGES "$HOME/.npm-packages"
+    # set -gx MANPATH "$NPM_PACKAGES/share/man:(manpath)"
+    # set -gx NODE_PATH "$NPM_PACKAGES/lib/node_modules"
 
     # python
     set -gx PYTHONDONTWRITEBYTECODE 1
 
     # dotnet
     set -gx DOTNET_CLI_TELEMETRY_OPTOUT 1
-    set -gx MONO_PATH "/Library/Frameworks/Mono.framework/Versions/Current/bin"
+    # set -gx MONO_PATH "/Library/Frameworks/Mono.framework/Versions/Current/bin"
 
     # gpg
     set -gx GPG_TTY "(tty)"
 
     # path
-    set -gx PATH $HOME/bin $HOME/miniconda3/bin /usr/local/opt/openssl/bin $NPM_PACKAGES/bin $HOME/.local/bin $HOME/.cargo/bin $HOME/.dotnet/tools $MONO_PATH $PATH
+    # set -gx PATH $HOME/bin $HOME/miniconda3/bin /usr/local/opt/openssl/bin $NPM_PACKAGES/bin $HOME/.local/bin $HOME/.cargo/bin $HOME/.dotnet/tools $MONO_PATH $PATH
+# set -gx PATH $HOME/bin $HOME/miniconda3/bin /usr/local/opt/openssl/bin $HOME/.local/bin $HOME/.cargo/bin $HOME/.dotnet/tools $PATH  # commented out by conda initialize
 
 else
     # alias
@@ -41,3 +43,9 @@ else
     alias pip="pip3"
     alias apt-update="sudo apt-get update; and sudo apt-get -y upgrade; and sudo apt-get clean"
 end
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /Users/calex/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+
